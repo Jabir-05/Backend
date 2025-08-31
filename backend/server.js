4db0ed60eb6510474c3e3da1e311f58aba4bc265
Jabir-05 <knock5jabir@gmail.com>
2025-09-01 00:23:11 +0530

    Connect

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
+
+// get a list of 5 jokes
+
+app.get("/api/jokes", (req, res) => {
+    const jokes = [
+        {
+            id1: 1,
+            title: "Why did the scarecrow win an award?",
+            joke: "Why don't scientists trust atoms? Because they make up everything!"
+
+        },
+        {
+            id2: 2,
+            title: "Why did the chicken join a band?",
+            joke: "Because it had the drumsticks!"  
+        },
+        {
+            id3: 3,
+            title: "Why did the math book look sad?",
+            joke: "Because it had too many problems."   
+        },
+        {
+            id4: 4,
+            title: "Why don't skeletons fight each other?",
+            joke: "They don't have the guts."   
+        }];
+    res.json(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server st http://localhost:${port}`);
+});
